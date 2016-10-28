be124903bdb74a13bba911d20fa0c374aaa8846e
kingdido999 <kingdido999@gmail.com>
2016-10-28 14:28:08 -0700

    correctly remove event listeners

--- src/image-zoom.js
+++ src/image-zoom.js
@@ -3,18 +3,19 @@
   function ImageZoomService() {
     this._scaleBase = 1.0
     this._image = null
-    this._overlay = null
     this._window = window
     this._document = document
     this._body = document.body
     this._last_known_scroll_position = 0
     this._ticking = false
+    this._handleClick = this._handleClick.bind(this)
+    this._handleKeyDown = this._handleKeyDown.bind(this)
+    this._handleScroll = this._handleScroll.bind(this)
   }
 
   ImageZoomService.prototype = {
     init: function() {
-      this._body.addEventListener('click', this._handleClick.bind(this))
-      this._overlay = new Overlay()
+      this._body.addEventListener('click', this._handleClick)
     },
 
     _zoom: function() {
@@ -22,18 +23,17 @@
         this._image.zoomIn(translate, scale)
       }).bind(this))
 
-      this._overlay.show()
-      this._document.addEventListener('keydown', this._handleKeyDown.bind(this))
-      this._document.addEventListener('scroll', this._handleScroll.bind(this))
+      this._document.addEventListener('keydown', this._handleKeyDown)
+      this._document.addEventListener('scroll', this._handleScroll)
     },
 
     _close: function() {
       if (!this._image) return
 
-      this._overlay.hide()
       this._image.zoomOut()
-      this._document.removeEventListener('keydown', this._handleKeyDown.bind(this))
-      this._document.removeEventListener('scroll', this._handleScroll.bind(this))
+
+      this._document.removeEventListener('keydown', this._handleKeyDown)
+      this._document.removeEventListener('scroll', this._handleScroll)
       this._image = null
     },
 
@@ -79,14 +79,11 @@
 
       if (!target) return
 
-      if (target.tagName !== 'IMG') this._close()
-
-      if (target.hasAttribute('data-action')) {
-        // Make the target image zoomable
-        this._image = new Zoomable(target)
-
+      if (target.tagName === 'IMG' && target.hasAttribute('data-action')) {
         switch (target.getAttribute('data-action')) {
           case 'zoom':
+            // Make the target image zoomable
+            this._image = new Zoomable(target)
             this._zoom()
             break;
           case 'close':
@@ -95,6 +92,8 @@
           default:
             break;
         }
+      } else {
+        this._close()
       }
     },
 
@@ -114,63 +113,62 @@
   }
 
   /**
-   * The overlay that hide/show DOM body.
-   */
-  function Overlay() {
-    this._body = document.body
-    this._element = document.createElement('div')
-    this._element.classList.add('image-zoom-overlay')
-  }
-
-  Overlay.prototype = {
-    show: function() {
-      this._body.classList.add('image-zoom-overlay-show')
-      this._body.appendChild(this._element)
-    },
-
-    hide: function() {
-      this._body.classList.remove('image-zoom-overlay-show')
-      this._body.removeChild(this._element)
-    }
-  }
-
-  /**
    * The target image.
    */
   function Zoomable(img) {
     this._target = img
+    this._overlay = null
     this._rect = this._target.getBoundingClientRect()
     this._body = document.body
+    this._handleTransitionEnd = this._handleTransitionEnd.bind(this)
   }
 
   Zoomable.prototype = {
     zoomIn: function(translate, scale) {
       this._target.setAttribute('data-action', 'close')
-      this._target.classList.add('image-zoom-transition', 'image-zoom-img')
+      this._target.classList.add('image-zoom-img')
+
+      this._overlay = document.createElement('div')
+      this._overlay.classList.add('image-zoom-overlay')
+      this._body.appendChild(this._overlay)
 
       var transform = 'translate(' + translate.x + 'px,' + translate.y + 'px) ' +
       'scale(' + scale + ',' + scale + ')'
+
 
       setStyles(this._target, {
         '-webkit-transform': transform,
         '-ms-transform': transform,
         'transform': transform,
       })
+
+      this._body.classList.add('image-zoom-overlay-show')
     },
 
     zoomOut: function() {
       this._target.setAttribute('data-action', 'zoom')
-      this._target.classList.remove('image-zoom-img')
+      this._target.addEventListener('transitionend', this._handleTransitionEnd)
 
       setStyles(this._target, {
         '-webkit-transform': '',
         '-ms-transform': '',
         'transform': '',
       })
+      
+      this._body.classList.remove('image-zoom-overlay-show')
     },
 
     getRect: function() {
       return this._rect
+    },
+
+    _handleTransitionEnd: function(event) {
+      if (this._target.getAttribute('data-action') === 'zoom') {
+        this._body.removeChild(this._overlay)
+        this._target.classList.remove('image-zoom-img')
+      }
+
+      this._target.removeEventListener('transitionend', this._handleTransitionEnd)
     }
   }
 
