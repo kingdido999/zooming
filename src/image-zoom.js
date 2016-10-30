dccc562cd5f981432e9b0b40d71bd38d027b5294
kingdido999 <kingdido999@gmail.com>
2016-10-30 09:01:45 -0700

    refactor

--- src/image-zoom.js
+++ src/image-zoom.js
@@ -3,10 +3,9 @@
   function ImageZoomService() {
     this._scaleBase = 1.0
     this._target = null // Zoomable target
-    this._window = window
-    this._document = document
     this._body = document.body
     this._lastScrollPosition = null
+    this._scrollThreshold = 40
 
     this._handleClick = this._handleClick.bind(this)
     this._handleKeyDown = this._handleKeyDown.bind(this)
@@ -18,11 +17,16 @@
       this._body.addEventListener('click', this._handleClick)
     },
 
-    _zoom: function() {
+    _zoom: function(target) {
+      // Avoid zooming multiple times
+      if (this._target) return
+
+      // Make the target image zoomable
+      this._target = new Zoomable(target)
       this._target.zoomIn()
 
-      this._document.addEventListener('keydown', this._handleKeyDown)
-      this._document.addEventListener('scroll', this._handleScroll)
+      document.addEventListener('keydown', this._handleKeyDown)
+      document.addEventListener('scroll', this._handleScroll)
     },
 
     _close: function() {
@@ -31,8 +35,8 @@
       this._target.zoomOut()
       this._target = null
 
-      this._document.removeEventListener('keydown', this._handleKeyDown)
-      this._document.removeEventListener('scroll', this._handleScroll)
+      document.removeEventListener('keydown', this._handleKeyDown)
+      document.removeEventListener('scroll', this._handleScroll)
     },
 
     _handleClick: function(event) {
@@ -40,21 +44,19 @@
 
       if (!target) return
 
-      if (target.tagName === 'IMG' && target.hasAttribute('data-action')) {
-        switch (target.getAttribute('data-action')) {
-          case 'zoom':
-            // Make the target image zoomable
-            this._target = new Zoomable(target)
-            this._zoom()
-            break
-          case 'close':
-            this._close()
-            break
-          default:
-            break
-        }
-      } else {
-        this._close()
+      if (target.tagName !== 'IMG') this._close()
+
+      if (!target.hasAttribute('data-action')) return
+
+      switch (target.getAttribute('data-action')) {
+        case 'zoom':
+          this._zoom(target)
+          break
+        case 'close':
+          this._close()
+          break
+        default:
+          break
       }
     },
 
@@ -63,12 +65,14 @@
     },
 
     _handleScroll: function() {
-      var scrollTop = this._window.pageYOffset ||
-      (this._document.documentElement || this._body.parentNode || this._body).scrollTop
+      var scrollTop = window.pageYOffset ||
+      (document.documentElement || this._body.parentNode || this._body).scrollTop
 
       if (this._lastScrollPosition === null) this._lastScrollPosition = scrollTop
+
       var deltaY = this._lastScrollPosition - scrollTop
-      if (Math.abs(deltaY) >= 40) {
+
+      if (Math.abs(deltaY) >= this._scrollThreshold) {
         this._lastScrollPosition = null
         this._close()
       }
@@ -76,19 +80,20 @@
   }
 
   /**
-   * The zoomable target.
+   * The zoomable image.
    */
-  function Zoomable(target) {
+  function Zoomable(img) {
     this._scaleBase = 1.0
-    this._target = target
-    this._src = target.getAttribute('src')
-    this._original = target.getAttribute('data-original')
+    this._image = img
+    this._src = img.getAttribute('src')
+    this._dataOriginal = img.getAttribute('data-original')
     this._overlay = null // An overlay that whites out the body
-    this._translate = null
-    this._scale = null
-    this._rect = this._target.getBoundingClientRect()
+    this._styles = {
+      width: '',
+      height: '',
+      transform: ''
+    }
     this._body = document.body
-    this._window = window
 
     this._handleTransitionEnd = this._handleTransitionEnd.bind(this)
     this._zoomOriginal = this._zoomOriginal.bind(this)
@@ -99,69 +104,46 @@
       var img = new Image()
 
       img.onload = (function() {
-        // If data-orginal is set, set image css width and height explicitly
+        // If data-orginal is present, set image css width and height explicitly
         // so the transformed source image is correctly displayed
-        if (this._original) {
-          this._target.style.width = img.width + 'px'
-          this._target.style.height = img.height + 'px'
+        if (this._dataOriginal) {
+          // Save the original image width and height styles if present
+          this._styles.width = this._image.style.width
+          this._styles.height = this._image.style.height
+
+          var rect = this._image.getBoundingClientRect()
+
+          setStyles(this._image, {
+            'width': rect.width + 'px',
+            'height': rect.height + 'px'
+          })
         }
 
         this._calculateZoom()
         this._zoomOriginal()
       }).bind(this)
 
-      img.src = this._target.src
-    },
-
-    _zoomOriginal: function() {
-      // Repaint before animating, fix Safari image rendering issue
-      this._target.offsetWidth
-
-      this._target.setAttribute('data-action', 'close')
-      this._target.classList.add('image-zoom-img')
-      this._target.addEventListener('transitionend', this._handleTransitionEnd)
-
-      // Zoom in the image
-      var transform = 'translate(' + this._translate.x + 'px,' + this._translate.y + 'px) ' +
-      'scale(' + this._scale + ',' + this._scale + ')'
-
-      setStyles(this._target, {
-        '-webkit-transform': transform,
-        '-ms-transform': transform,
-        'transform': transform,
-      })
-
-      this._overlay = document.createElement('div')
-
-      // If add class to overlay before transforming the image,
-      // it will cause image flickering on Safari.
-      this._overlay.classList.add('image-zoom-overlay')
-
-      this._body.appendChild(this._overlay)
-
-      // Use setTimeout to apply correct body opacity transition when
-      // zooming in, otherwise the transition effect won't trigger.
-      this._window.setTimeout((function() {
-        this._body.classList.add('image-zoom-overlay-show')
-      }).bind(this), 50)
+      img.src = this._src
     },
 
     _calculateZoom: function() {
+      var rect = this._image.getBoundingClientRect()
+
       var windowCenter = {
-        x: this._window.innerWidth / 2,
-        y: this._window.innerHeight / 2
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
       }
 
-      var imgHalfWidth = this._rect.width / 2
-      var imgHalfHeight = this._rect.height / 2
+      var imgHalfWidth = rect.width / 2
+      var imgHalfHeight = rect.height / 2
 
       var imgCenter = {
-        x: this._rect.left + imgHalfWidth,
-        y: this._rect.top + imgHalfHeight
+        x: rect.left + imgHalfWidth,
+        y: rect.top + imgHalfHeight
       }
 
       // The vector to translate image to the window center
-      this._translate = {
+      var translate = {
         x: windowCenter.x - imgCenter.x,
         y: windowCenter.y - imgCenter.y
       }
@@ -176,44 +158,87 @@
       // scaling horizontally and scaling vertically
       var scaleHorizontally = distFromImageEdgeToWindowEdge.x / imgHalfWidth
       var scaleVertically = distFromImageEdgeToWindowEdge.y / imgHalfHeight
-      this._scale = this._scaleBase + Math.min(scaleHorizontally, scaleVertically)
+      var scale = this._scaleBase + Math.min(scaleHorizontally, scaleVertically)
+
+      // Translate the image to window center, then scale the image
+      this._styles.transform =
+        'translate(' + translate.x + 'px,' + translate.y + 'px) ' +
+        'scale(' + scale + ',' + scale + ')'
+    },
+
+    _zoomOriginal: function() {
+      // Repaint before animating, fix Safari image flickring issue
+      this._image.offsetWidth
+
+      this._image.classList.add('image-zoom-img')
+      this._image.addEventListener('transitionend', this._handleTransitionEnd)
+
+      // Create an overlay, it does not white out at this point
+      this._overlay = document.createElement('div')
+      this._overlay.classList.add('image-zoom-overlay')
+      this._body.appendChild(this._overlay)
+
+      // Use setTimeout to apply correct overlay opacity transition when
+      // zooming in, otherwise the transition effect won't trigger.
+      window.setTimeout((function() {
+        // Now whites out the overlay
+        setStyles(this._overlay, {
+          'filter': "alpha(opacity=100)",
+          'opacity': 1
+        })
+      }).bind(this), 30)
+
+      this._transform()
     },
 
     zoomOut: function() {
-      this._target.setAttribute('data-action', 'zoom')
-      this._target.addEventListener('transitionend', this._handleTransitionEnd)
-      this._body.classList.remove('image-zoom-overlay-show')
+      this._image.addEventListener('transitionend', this._handleTransitionEnd)
 
-      // Zoom out the image
-      setStyles(this._target, {
-        '-webkit-transform': '',
-        '-ms-transform': '',
-        'transform': '',
+      if (this._dataOriginal) {
+        this._image.setAttribute('src', this._src)
+      }
+
+      // Remove the overlay
+      setStyles(this._overlay, {
+        'filter': "alpha(opacity=0)",
+        'opacity': 0
       })
 
-      if (this._original) {
-        this._target.setAttribute('src', this._src)
-      }
+      this._styles.transform = ''
+      this._transform()
+    },
+
+    _transform: function() {
+      setStyles(this._image, {
+        '-webkit-transform': this._styles.transform,
+        '-ms-transform': this._styles.transform,
+        'transform': this._styles.transform,
+      })
     },
 
     _handleTransitionEnd: function(event) {
-      switch (this._target.getAttribute('data-action')) {
-        case 'zoom':
-          this._body.removeChild(this._overlay)
-          this._target.classList.remove('image-zoom-img')
-          break
+      switch (this._image.getAttribute('data-action')) {
         case 'close':
-          if (this._original) {
-            this._target.setAttribute('src', this._original)
-            // this._target.style.width = this._rect.width + 'px'
-            // this._target.style.height = this._rect.height + 'px'
+          if (this._dataOriginal) {
+            setStyles(this._image, {
+              'width': this._styles.width,
+              'height': this._styles.height
+            })
           }
+          this._body.removeChild(this._overlay)
+          this._image.classList.remove('image-zoom-img')
+          this._image.setAttribute('data-action', 'zoom')
+          break
+        case 'zoom':
+          if (this._dataOriginal) {
+            this._image.setAttribute('src', this._dataOriginal)
+          }
+          this._image.setAttribute('data-action', 'close')
           break
         default:
           break
       }
-
-      this._target.removeEventListener('transitionend', this._handleTransitionEnd)
+      this._image.removeEventListener('transitionend', this._handleTransitionEnd)
     }
   }
 
