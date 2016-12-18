2554b1463a2c33b34f2337be7abd15300f9d9e63
kingdido999 <kingdido999@gmail.com>
2016-12-17 21:49:52 -0800

    suppress rollup warning

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import babel from 'rollup-plugin-babel'
 import eslint from 'rollup-plugin-eslint'
 
 export default {
-  entry: `src/zooming.js`,
-  dest: `build/zooming.js`,
+  entry: 'src/zooming.js',
+  dest: 'build/zooming.js',
   format: 'umd',
   moduleName: 'Zooming',
   sourceMap: true,
@@ -12,5 +12,12 @@ export default {
       exclude: 'node_modules/**'
     }),
     eslint({})
-  ]
+  ],
+  onwarn: function (message) {
+    // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+    // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+    if (/The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten./.test(message)) {
+        return
+    }
+  },
 }
--- rollup.config.uglify.js
+++ rollup.config.uglify.js
@@ -3,8 +3,8 @@ import eslint from 'rollup-plugin-eslint'
 import uglify from 'rollup-plugin-uglify'
 
 export default {
-  entry: `src/zooming.js`,
-  dest: `build/zooming.min.js`,
+  entry: 'src/zooming.js',
+  dest: 'build/zooming.min.js',
   format: 'umd',
   moduleName: 'Zooming',
   sourceMap: true,
@@ -14,5 +14,12 @@ export default {
     }),
     eslint({}),
     uglify()
-  ]
+  ],
+  onwarn: function (message) {
+    // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
+    // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
+    if (/The 'this' keyword is equivalent to 'undefined' at the top level of an ES module, and has been rewritten./.test(message)) {
+        return
+    }
+  },
 }
