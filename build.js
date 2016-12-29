ebc29de703aaad8e7e0635ec88f4951cbbf1bb22
kingdido999 <kingdido999@gmail.com>
2016-12-28 16:51:40 -0800

    update docs

--- build.js
+++ build.js
@@ -27,7 +27,7 @@ const defaultPlugins = [
 
 const config = (dest, plugins) => {
   return {
-    entry: 'src/zooming.js',
+    entry: 'src/main.js',
     dest: dest,
     format: 'umd',
     moduleName: 'Zooming',
@@ -42,7 +42,6 @@ const config = (dest, plugins) => {
   }
 }
 
-// stderr to stderr to keep `rollup zooming.js > bundle.js` from breaking
 const stderr = console.error.bind(console) // eslint-disable-line no-console
 
 const eventHandler = (event) => {
