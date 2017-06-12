f3eb8c45dd4bd78ef40f161ac076da4747cea0ee
kingdido999 <kingdido999@gmail.com>
2017-06-12 13:51:52 -0700

    Clean up test

--- test/index.js
+++ test/index.js
@@ -4,80 +4,78 @@ var prefix = 'WebkitAppearance' in document.documentElement.style
   : ''
 
 describe('API', function() {
-  var testZooming = new Zooming()
-  var defaultOpts = Object.assign({}, testZooming.config())
-  var els = document.querySelectorAll(defaultOpts.defaultZoomable)
-  var el = els[0]
+  var z = new Zooming()
+  var defaultOpts = Object.assign({}, z.config())
+  var el = document.querySelector(defaultOpts.defaultZoomable)
+  var opts = {
+    defaultZoomable: 'img[data-action="open"]',
+    enableGrab: false,
+    preloadImage: false,
+    transitionDuration: 0.2,
+    transitionTimingFunction: 'ease',
+    bgColor: '#000',
+    bgOpacity: 0.8,
+    scaleBase: 0.8,
+    scaleExtra: 0.3,
+    scrollThreshold: 50,
+    zIndex: 777,
+    customSize: { width: 800, height: 400 },
+    onOpen: function() {
+      return 1
+    },
+    onClose: function() {
+      return 1
+    },
+    onRelease: function() {
+      return 1
+    },
+    onBeforeOpen: function() {
+      return 1
+    },
+    onBeforeClose: function() {
+      return 1
+    },
+    onBeforeGrab: function() {
+      return 1
+    },
+    onBeforeRelease: function() {
+      return 1
+    }
+  }
 
-  describe('listen()', function() {
+  describe('listen', function() {
     it('should set cursor style to zoom-in', function() {
       expect(el.style.cursor).to.equal(prefix + 'zoom-in')
     })
   })
 
-  describe('config()', function() {
-    var opts = {
-      defaultZoomable: 'img[data-action="open"]',
-      enableGrab: false,
-      preloadImage: false,
-      transitionDuration: 0.2,
-      transitionTimingFunction: 'ease',
-      bgColor: '#000',
-      bgOpacity: 0.8,
-      scaleBase: 0.8,
-      scaleExtra: 0.3,
-      scrollThreshold: 50,
-      zIndex: 998,
-      customSize: { width: 800, height: 400 },
-      onOpen: function() {
-        return 'onOpen'
-      },
-      onClose: function() {
-        return 'onClose'
-      },
-      onRelease: function() {
-        return 'onRelease'
-      },
-      onBeforeOpen: function() {
-        return 'onBeforeOpen'
-      },
-      onBeforeClose: function() {
-        return 'onBeforeClose'
-      },
-      onBeforeGrab: function() {
-        return 'onBeforeGrab'
-      },
-      onBeforeRelease: function() {
-        return 'onBeforeRelease'
-      }
-    }
-
+  describe('config', function() {
     before(function() {
-      testZooming.config(opts)
+      z.config(opts)
     })
 
     it('should update options correctly', function() {
-      expect(testZooming.config()).to.deep.equal(opts)
+      expect(z.config()).to.deep.equal(opts)
     })
 
     after(function() {
-      testZooming.config(defaultOpts)
+      z.config(defaultOpts)
     })
   })
 
-  describe('config() new instance', function() {
-    var testZooming2 = new Zooming({})
-    var defaultZoomable = 'img[data-action="zoom2"]'
-    var scrollThreshold = 250
+  describe('config for a new instance', function() {
+    var y = new Zooming()
     var newOptions = {
-      defaultZoomable: defaultZoomable,
-      scrollThreshold: scrollThreshold
+      defaultZoomable: 'img[data-action="open"]',
+      scrollThreshold: 250
     }
 
-    testZooming2.config(newOptions)
+    before(function() {
+      y.config(newOptions)
+    })
 
-    it('should update options correctly', function() {
-      var resultOptions = testZooming2.config()
+    it('should update options for the new instance correctly', function() {
+      var resultOptions = y.config()
 
       for (var key in resultOptions) {
         if (key in newOptions) {
@@ -89,11 +87,13 @@ describe('API', function() {
     })
   })
 
-  describe('open()', function() {
+  describe('open', function() {
     it('should open up the image', function(done) {
-      testZooming.open(el, function(target) {
+      z.open(el, function(target) {
         expect(target.style.position).to.equal('relative')
-        expect(target.style.zIndex).to.equal('999')
+        expect(target.style.zIndex).to.equal(
+          (defaultOpts.zIndex + 1).toString()
+        )
         expect(target.style.cursor).to.equal(
           prefix + (defaultOpts.enableGrab ? 'grab' : 'zoom-out')
         )
@@ -106,8 +106,9 @@ describe('API', function() {
     it('should insert the overlay', function() {
       var overlay = document.body.lastElementChild
       expect(overlay.tagName).to.equal('DIV')
+
       var style = overlay.style
-      expect(style.zIndex).to.equal('998')
+      expect(style.zIndex).to.equal(defaultOpts.zIndex.toString())
       expect(style.backgroundColor).to.equal(defaultOpts.bgColor)
       expect(style.position).to.equal('fixed')
       expect(style.top).to.equal('0px')
@@ -119,12 +120,12 @@ describe('API', function() {
     })
   })
 
-  describe('grab()', function() {
-    var x = window.innerWidth / 2
-    var y = window.innerHeight / 2
-
+  describe('grab', function() {
     it('should grab the image', function(done) {
-      testZooming.grab(x, y, defaultOpts.scaleExtra, function(target) {
+      var x = window.innerWidth / 2
+      var y = window.innerHeight / 2
+
+      z.grab(x, y, defaultOpts.scaleExtra, function(target) {
         expect(target.style.cursor).to.equal('move')
         expect(target.style.transition).to.not.be.empty
         expect(target.style.transform).to.not.be.empty
@@ -133,11 +134,13 @@ describe('API', function() {
     })
   })
 
-  describe('release()', function() {
+  describe('release', function() {
     it('should release the image', function(done) {
-      testZooming.release(function(target) {
+      z.release(function(target) {
         expect(target.style.position).to.equal('relative')
-        expect(target.style.zIndex).to.equal('999')
+        expect(target.style.zIndex).to.equal(
+          (defaultOpts.zIndex + 1).toString()
+        )
         expect(target.style.cursor).to.equal(
           prefix + (defaultOpts.enableGrab ? 'grab' : 'zoom-out')
         )
@@ -148,9 +151,9 @@ describe('API', function() {
     })
   })
 
-  describe('close()', function() {
+  describe('close', function() {
     it('should close out the image', function(done) {
-      testZooming.close(function(target) {
+      z.close(function(target) {
         expect(target.style.position).to.be.empty
         expect(target.style.zIndex).to.be.empty
         expect(target.style.cursor).to.equal(prefix + 'zoom-in')
